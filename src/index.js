e7f270e8ffea285729f6ef7a2cab76124e679b55
Jheisson-santos <jheisson.g.santos@gmail.com>
2024-09-23 18:54:02 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import Produtora from "./routers/produtora.js"; 
 import Diretor from "./routers/diretor.js";
+import Filme from "./routers/filme.js"
 const port = 3000
 const app = express();
 
 app.use(express.json());
 app.use("/produtora", Produtora)
 app.use("/diretor", Diretor)
+app.use("/filme", Filme)
 
 
 app.listen(port, ()=>{
